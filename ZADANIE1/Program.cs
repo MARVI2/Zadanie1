fd52eec95c3feab4551144a444734ac221a3f90a
Kornel G <kor.garcz@gmail.com>
2025-02-21 08:42:19 +0100

    ZADANIE1 do zmiany

---
+++ ZADANIE1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ZADANIE1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string path = "C:\\Users\\bartl\\OneDrive\\Pulpit\\praktyki\\ZADANIE1\\ZADANIE1\\test_KOR_GAR.txt";
+            int counter = 0;
+
+            if (!File.Exists(path)) return;
+
+            StreamReader sw = File.OpenText(path);
+            string line;
+
+            while(( line = sw.ReadLine()) != null){
+                foreach(char letter in line)
+                {
+                    if (letter == 'a') counter++;
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
